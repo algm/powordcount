e209eef3427163ecb908dc3bb4b2d6c04b3f411d
Antonio Gázquez <algm85@gmail.com>
2020-04-24 19:10:38 +0200

    Fix styles

--- src/components/FileList.js
+++ src/components/FileList.js
@@ -46,9 +46,9 @@ function FileList({ files }) {
   };
 
   return (
-    <div className="overflow-y-auto text-left space-y-3 items-center">
+    <div className="overflow-y-auto text-left space-y-3 items-center justify-center">
       <div className="flex flex-row space-x-2">
-        <div className="flex flex-row space-x-2 flex-1 items-start justify-center">
+        <div className="flex flex-row space-x-2 flex-1 items-start items-center">
           <div className="py-2 pr-2">
             <strong>Total words</strong>
           </div>
